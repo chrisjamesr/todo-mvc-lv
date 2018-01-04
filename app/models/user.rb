6eda3b272c7308892c9efd5737added7e6e0cad1
chrisjamesr <chrisjames.r@gmail.com>
2018-01-04 13:00:25 -0500

    add find or create by omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   validates :email, :uniqueness => true
 
-end
+  # Devise Method
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.email =  auth.email
+  #     user.password = SecureRandom.hex
+  #   end  
+  # end
+
+
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(:email => auth_hash["info"]["email"]).first_of_create do |user|
+      user.password = SecureRandom.hex
+    end  
+  end   
+
+end  #  End of Class
