6e412abcb85ab01c6bef430b1f672d99cce152d8
chrisjamesr <chrisjames.r@gmail.com>
2018-01-03 22:56:11 -0500

    build out session create method using omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+
 class SessionsController < ApplicationController
 
   def new
@@ -5,19 +6,42 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(:email => params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if auth_hash = request.env["omniauth.auth"]
       
-      redirect_to root_path
-    else
-      
-      redirect_to new_user_path 
+      #logged in via omniauth
+     oauth_email = request.env["omniauth.auth"]["info"]["email"]
+     if user = User.find_by(:email => oauth_email)
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        user = User.new(:email => oauth_email, :password => SecureRandom.hex)
+        if user.save
+          session[:user_id] = user.id
+          redirect_to root_path
+        else
+          raise user.errors.full_messages.inspect 
+        end
+      end
+    elsif
+      user = User.find_by(:email => params[:email])
+        #normal login with username & pw
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect_to root_path 
+        end
+      else  
+        render 'sessions/new'
     end
   end
 
   def destroy
     reset_session
     redirect_to login_path
-  end
+  end 
+  
+
+  private
+    # def auth
+    #   request.env['omniauth.auth']
+    # end
 end
