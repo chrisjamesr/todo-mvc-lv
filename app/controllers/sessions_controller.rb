1a7d154a767b8f58d6834b5698c0a936ffdd07f0
chrisjamesr <chrisjames.r@gmail.com>
2018-01-04 13:19:54 -0500

    refactor sessions#create to use find_or_create_by_omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,30 +7,11 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash = request.env["omniauth.auth"]
-      
-      #logged in via omniauth
-     oauth_email = request.env["omniauth.auth"]["info"]["email"]
-     if user = User.find_by(:email => oauth_email)
-        session[:user_id] = user.id
-        redirect_to root_path
-      else
-        user = User.new(:email => oauth_email, :password => SecureRandom.hex)
-        if user.save
-          session[:user_id] = user.id
-          redirect_to root_path
-        else
-          raise user.errors.full_messages.inspect 
-        end
-      end
-    elsif
-      user = User.find_by(:email => params[:email])
-        #normal login with username & pw
-        if user && user.authenticate(params[:password])
-          session[:user_id] = user.id
-          redirect_to root_path 
-        end
-      else  
-        render 'sessions/new'
+      user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = user.id
+      redirect_to root_path
+    else  
+      render 'sessions/new'
     end
   end
 
@@ -40,8 +21,5 @@ class SessionsController < ApplicationController
   end 
   
 
-  private
-    # def auth
-    #   request.env['omniauth.auth']
-    # end
+      
 end
